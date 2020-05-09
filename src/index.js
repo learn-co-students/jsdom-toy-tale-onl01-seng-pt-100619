5ca7e6dff3a33bc45e4885bb8a032c0b265d6ffc
allisoncortez <allizetorc@gmail.com>
2020-05-08 20:50:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  addToyForm()
+  fetchToys()
+});
+
+function addToyForm(){
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +17,52 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+}
+
+  function fetchToys(){
+    //GET request for all toy objects
+    fetch("http://localhost:3000/toys")
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((obj) => {
+      for (const toy of obj) {
+        createToyCard(toy)
+      }
+    })
+  }
+
+  function createToyCard(toy){
+    const toyCollection = document.getElementById('toy-collection')
+    const card = document.createElement('div')
+    card.classList.add("card")
+    
+    //create header
+    const cardHeader = document.createElement('h2')
+    cardHeader.innerHTML = toy.name
+    card.appendChild(cardHeader)
+    //create avatar
+    const avatar = document.createElement('img')
+    avatar.classList = "toy-avatar"
+    avatar.src = toy.image
+    card.appendChild(avatar)
+    // # of Likes
+    const numLikes = document.createElement('p')
+    numLikes.innerHTML = `${toy.likes} Likes`
+    card.appendChild(numLikes)
+
+    //Like Button
+    const likeBtn = document.createElement('button')
+    likeBtn.classList = "like-btn"
+    likeBtn.innerText = "Like <3"
+    card.appendChild(likeBtn)
+    
+
+    
+
+
+
+
+    //add card to collection
+    toyCollection.appendChild(card)
+  }
